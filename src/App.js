d2c45c32e354025839104af22a76723b531166fb
andriali044 <andriali044gmail.com>
2022-08-18 10:46:16 +0700

    tes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Coba from './Coba'
+function App() {
+  return (
+    <div className="App">
+      <Coba />
+    </div>
+  );
+}
+
+export default App;
